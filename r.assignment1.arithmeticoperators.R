b17811c1777583d782d94953ebe060ad58c387c2
era0506 <aradhya.uni05@gmail.com>
2025-12-08 03:56:52 +0530

    Add files via upload
    Assignment1 : Arithmetic operator

---
+++ r.assignment1.arithmeticoperators.R
@@ -0,0 +1,16 @@
+#Tasks Performed:
+#1. Perform the following operations on the dataset using arithmetic operator:
+#a. Increase the monthly charges of the 1st record by 5
+customer.churn05$MonthlyCharges[1] +5 -> customer.churn05$MonthlyCharges[1]
+#b. Increase the monthly charges of the 5th record by 22
+customer.churn05$MonthlyCharges[5] +22 -> customer.churn05$MonthlyCharges[5]
+#c. Reduce the total charges of the 4th record by 40.75
+customer.churn05$TotalCharges[4] -40.75 -> customer.churn05$TotalCharges[4]
+#d. Subtract the total charges from 2nd record by 100
+customer.churn05$TotalCharges[2] -100 -> customer.churn05$TotalCharges[2]
+#e. Reduce the monthly charges of the 25th record by 4.6 and divide it by 5
+(customer.churn05$MonthlyCharges[25] -4.6) / 5 -> customer.churn05$MonthlyCharges[25]
+#f. Increase the value of the total charge of the 5th record by 5.3 and multiply it by 2
+(customer.churn05$TotalCharges[5] +5.3) * 2 -> customer.churn05$TotalCharges[5]
+#g. Multiply the 12th record of monthly charges by 5
+customer.churn05$MonthlyCharges[12]*5 -> customer.churn05$MonthlyCharges[12]
